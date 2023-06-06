1b032beb4e2ce450107eb55fdb0db9cff8d95d40
Bennyzhu1 <zhu.ben@northeastern.edu>
2023-06-06 17:59:51 -0400

    Initial record for ReportDamage

--- src/main/java/cs3500/pa04/json/SuccessfulHitsJson.java
+++ src/main/java/cs3500/pa04/json/SuccessfulHitsJson.java
@@ -1,11 +1,10 @@
 package cs3500.pa04.json;
 
-
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.JsonNode;
 import cs3500.pa04.model.Coord;
 import java.util.List;
 
-public record ReportDamageJson(@JsonProperty("report-damage") List<Coord> shots,
-                               @JsonProperty("arguments") JsonNode arguments) {
+public record SuccessfulHitsJson(@JsonProperty("successful-hits") List<Coord> shots,
+                                 @JsonProperty("arguments") JsonNode arguments) {
 }
