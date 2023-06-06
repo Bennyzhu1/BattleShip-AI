35f8d6f996db36052b2c2db90f1c17d78f7b2eb1
Kiyo <68307546+KiyonoKara@users.noreply.github.com>
2023-06-06 18:05:43 -0400

    Work on ProxyController

--- src/main/java/cs3500/pa04/client/ProxyController.java
+++ src/main/java/cs3500/pa04/client/ProxyController.java
@@ -1,7 +1,43 @@
 package cs3500.pa04.client;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import cs3500.pa04.json.MessageJson;
+import cs3500.pa04.model.Player;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.net.Socket;
+
 /**
- * Dispatches messages and data from the server
+ * Dispatches messages and data from the server; responds to server
  */
 public class ProxyController {
+  private final Socket server;
+  private final InputStream in;
+  private final PrintStream out;
+  private final Player player;
+  private final ObjectMapper mapper = new ObjectMapper();
+
+  public ProxyController(Socket server, Player player) throws IOException {
+    this.server = server;
+    this.in = server.getInputStream();
+    this.out = new PrintStream(server.getOutputStream());
+    this.player = player;
+  }
+
+  public void run() {
+    try {
+      JsonParser jsonParser = this.mapper.getFactory().createParser(this.in);
+
+      while (!this.server.isClosed()) {
+        MessageJson message = jsonParser.readValueAs(MessageJson.class);
+        // TODO: Delegate messages
+      }
+
+    } catch (IOException e) {
+      throw new RuntimeException("Disconnected from server or failed to parse");
+    }
+  }
 }
