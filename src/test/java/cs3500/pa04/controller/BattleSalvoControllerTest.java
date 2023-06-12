6e67f8a3e41ffac8cb71cd61e4c3f9974bc10ac1
Kiyo <68307546+KiyonoKara@users.noreply.github.com>
2023-06-11 21:03:24 -0400

    Fix tests for the controller.

--- src/test/java/cs3500/pa04/controller/BattleSalvoControllerTest.java
+++ src/test/java/cs3500/pa04/controller/BattleSalvoControllerTest.java
@@ -3,12 +3,10 @@ package cs3500.pa04.controller;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import cs3500.pa04.MockAiPlayer;
 import cs3500.pa04.MockManualPlayer;
 import cs3500.pa04.model.AiPlayer;
-import cs3500.pa04.model.BetterAiPlayer;
 import cs3500.pa04.model.Coord;
 import cs3500.pa04.model.MoveData;
 import java.io.ByteArrayInputStream;
